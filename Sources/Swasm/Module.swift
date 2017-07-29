984aafc3c4ffffe957f6ac5840e02dcb7c8543c6
Akio Yasui <akkyie01@gmail.com>
2017-07-30 07:53:04 +0900

    Confirm types of Module to Equatable

--- Sources/Swasm/Module.swift
+++ Sources/Swasm/Module.swift
@@ -15,8 +15,20 @@ struct Table {
 	let type: TableType
 }
 
+extension Table: Equatable {
+	static func == (lhs: Table, rhs: Table) -> Bool {
+		return lhs.type == rhs.type
+	}
+}
+
 struct Memory {
 	let type: MemoryType
+}
+
+extension Memory: Equatable {
+	static func == (lhs: Memory, rhs: Memory) -> Bool {
+		return lhs.type == rhs.type
+	}
 }
 
 struct Global {
@@ -24,10 +36,22 @@ struct Global {
 	let initializer: Expression
 }
 
+extension Global: Equatable {
+	static func == (lhs: Global, rhs: Global) -> Bool {
+		return (lhs.type, lhs.initializer) == (rhs.type, rhs.initializer)
+	}
+}
+
 struct Element {
 	let table: TableIndex
 	let offset: Expression
 	let initializer: [FunctionIndex]
+}
+
+extension Element: Equatable {
+	static func == (lhs: Element, rhs: Element) -> Bool {
+		return (lhs.table, lhs.offset) == (rhs.table, rhs.offset) && lhs.initializer == rhs.initializer
+	}
 }
 
 struct Data {
@@ -41,11 +65,34 @@ struct Export {
 	let descriptor: ExportDescriptor
 }
 
+extension Export: Equatable {
+	static func == (lhs: Export, rhs: Export) -> Bool {
+		return (lhs.name, lhs.descriptor) == (rhs.name, rhs.descriptor)
+	}
+}
+
 enum ExportDescriptor {
 	case function(FunctionIndex)
 	case table(TableIndex)
 	case memory(MemoryIndex)
 	case global(GlobalIndex)
+}
+
+extension ExportDescriptor: Equatable {
+	static func == (lhs: ExportDescriptor, rhs: ExportDescriptor) -> Bool {
+		switch (lhs, rhs) {
+		case let (.function(l), .function(r)):
+			return l == r
+		case let (.table(l), .table(r)):
+			return l == r
+		case let (.memory(l), .memory(r)):
+			return l == r
+		case let (.global(l), .global(r)):
+			return l == r
+		default:
+			return false
+		}
+	}
 }
 
 struct Import {
@@ -54,9 +101,32 @@ struct Import {
 	let descripter: ImportDescriptor
 }
 
+extension Import: Equatable {
+	static func == (lhs: Import, rhs: Import) -> Bool {
+		return (lhs.module, lhs.name, lhs.descripter) == (rhs.module, rhs.name, rhs.descripter)
+	}
+}
+
 enum ImportDescriptor {
 	case function(TypeIndex)
 	case table(TableType)
 	case memory(MemoryType)
 	case global(GlobalType)
+}
+
+extension ImportDescriptor: Equatable {
+	static func == (lhs: ImportDescriptor, rhs: ImportDescriptor) -> Bool {
+		switch (lhs, rhs) {
+		case let (.function(l), .function(r)):
+			return l == r
+		case let (.table(l), .table(r)):
+			return l == r
+		case let (.memory(l), .memory(r)):
+			return l == r
+		case let (.global(l), .global(r)):
+			return l == r
+		default:
+			return false
+		}
+	}
 }
