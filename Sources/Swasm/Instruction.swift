156008ee7b06f05bd15347573ee0c52f9bcdb5b7
Akio Yasui <akkyie01@gmail.com>
2017-07-30 07:52:46 +0900

    Conform Instruction and Expression to Equatable

--- Sources/Swasm/Instruction.swift
+++ Sources/Swasm/Instruction.swift
@@ -1,7 +1,33 @@
 struct Expression {
-	var instructions: [Instruction]
+	var instructions: [AnyInstruction]
+}
+
+extension Expression: Equatable {
+	static func == (lhs: Expression, rhs: Expression) -> Bool {
+		return lhs.instructions == rhs.instructions
+	}
 }
 
 protocol Instruction {
 	var isConstant: Bool { get }
+
+	func isEqual(to another: Instruction) -> Bool
+}
+
+struct AnyInstruction: Instruction {
+	var isConstant: Bool {
+		return instruction.isConstant
+	}
+
+	let instruction: Instruction
+
+	func isEqual(to another: Instruction) -> Bool {
+		return instruction.isEqual(to: instruction)
+	}
+}
+
+extension AnyInstruction: Equatable {
+	static func == (lhs: AnyInstruction, rhs: AnyInstruction) -> Bool {
+		return lhs.instruction.isEqual(to: rhs.instruction)
+	}
 }
