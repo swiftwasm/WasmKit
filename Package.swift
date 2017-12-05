ce472248bdd36bd78759decec585678a3ccec174
Akio Yasui <akkyie01@gmail.com>
2017-12-05 12:10:18 +0900

    Use SwiftFormat

--- Package.swift
+++ Package.swift
@@ -8,11 +8,11 @@ public let package = Package(
         .library(
             name: "Swasm",
             targets: ["Swasm"]),
-        ],
+    ],
     dependencies: [
         .package(url: "https://github.com/akkyie/antlr4-swift.git", from: "4.0.0"),
         .package(url: "https://github.com/antitypical/Result.git", from: "3.2.0"),
-        ],
+    ],
     targets: [
         .target(
             name: "Swasm",
@@ -20,6 +20,6 @@ public let package = Package(
         .testTarget(
             name: "SwasmTests",
             dependencies: ["Swasm"]),
-        ],
+    ],
     swiftLanguageVersions: [4]
 )
--- scripts/add_lint_phase.rb
+++ scripts/add_lint_phase.rb
@@ -5,29 +5,55 @@ require 'xcodeproj'
 project_path = File.expand_path('../../Swasm.xcodeproj', __FILE__)
 project = Xcodeproj::Project.open(project_path)
 
-TARGET_NAME = "Swasm"
-PHASE_NAME = "Run SwiftLint"
+targets = ["Swasm", "SwasmTests"]
 
-project.targets.each do |target|
-  next unless target.name == TARGET_NAME
-
-  next if target.build_phases.any? do |phase|
-    phase.respond_to?(:name) and phase.name == PHASE_NAME
+def add_swiftlint(project, target, phase_name)
+  return if target.build_phases.any? do |phase|
+    phase.respond_to?(:name) and phase.name == phase_name
   end
 
   phase = project.new(Xcodeproj::Project::Object::PBXShellScriptBuildPhase)
-  phase.name = PHASE_NAME
+  phase.name = phase_name
   phase.shell_script = <<-EOS
   if which swiftlint >/dev/null; then
     swiftlint
   else
-    echo "warning: SwiftLint not installed, download from https://github.com/realm/SwiftLint"
+    echo "warning: SwiftLint not installed; download it from https://github.com/realm/SwiftLint"
   fi
   EOS
 
   target.build_phases.unshift(phase)
+
+  puts "Added a build phase named \"#{phase_name}\""
+end
+
+def add_swiftformat(project, target, phase_name)
+  return if target.build_phases.any? do |phase|
+    phase.respond_to?(:name) and phase.name == phase_name
+  end
+
+  phase = project.new(Xcodeproj::Project::Object::PBXShellScriptBuildPhase)
+  phase.name = phase_name
+  phase.shell_script = <<-EOS
+  if which swiftformat >/dev/null; then
+  swiftformat . --verbose
+  else
+    echo "warning: SwiftFormat not installed; download it from https://github.com/nicklockwood/SwiftFormat"
+  fi
+  EOS
+
+  target.build_phases.unshift(phase)
+
+  puts "Added a build phase named \"#{phase_name}\""
+end
+
+
+project.targets.each do |target|
+  next unless targets.include? target.name
+
+  add_swiftlint(project, target, "Run SwiftLint")
+  add_swiftformat(project, target, "Run SwiftFormat")
 end
 
 project.save()
 
-puts "Added a build phase named \"#{PHASE_NAME}\""
