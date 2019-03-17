922fb987465f87cfb07fdff4ae673ed462d3e4d1
Akio Yasui <akkyie01@gmail.com>
2019-03-18 08:49:26 +0900

    Remove Quick and Nimble

--- Package.swift
+++ Package.swift
@@ -19,8 +19,6 @@ let package = Package(
         .package(url: "https://github.com/jakeheis/SwiftCLI", from: "5.0.0"),
         .package(url: "https://github.com/onevcat/Rainbow", from: "3.1.4"),
         .package(url: "https://github.com/Nike-Inc/Willow", from: "5.1.0"),
-        .package(url: "https://github.com/Quick/Quick", from: "2.0.0"),
-        .package(url: "https://github.com/Quick/Nimble", from: "8.0.0"),
     ],
     targets: [
         .target(
@@ -30,7 +28,7 @@ let package = Package(
         ),
         .testTarget(
             name: "WAKitTests",
-            dependencies: ["WAKit", "Quick", "Nimble"],
+            dependencies: ["WAKit"],
             path: "./Tests/WAKitTests"
         ),
         .target(
