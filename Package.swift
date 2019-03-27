9e4bc567173047dd42594f8336633da4d1d32d73
Akio Yasui <akkyie01@gmail.com>
2019-03-27 21:28:18 +0900

    Swift 5.0

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.0
 
 import PackageDescription
 
@@ -37,5 +37,5 @@ let package = Package(
             path: "./Sources/CLI"
         ),
     ],
-    swiftLanguageVersions: [4]
+    swiftLanguageVersions: [.v5]
 )
