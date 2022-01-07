5328d67106eec13a691b939388b5c90c937759b0
Max Desiatov <max@desiatov.com>
2022-01-07 08:57:01 +0000

    Migrate from SwiftCLI to Swift Argument Parser (#50)
    Swift Argument Parser is more popular and has more maintenance guarantees, since it's utilized in multiple core tools such as SourceKit-LSP and even SwiftPM itself.

--- Package.swift
+++ Package.swift
@@ -16,25 +16,27 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/akkyie/SwiftLEB", from: "0.1.0"),
-        .package(url: "https://github.com/jakeheis/SwiftCLI", from: "5.0.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.2"),
         .package(url: "https://github.com/onevcat/Rainbow", from: "3.1.4"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
     ],
     targets: [
         .target(
             name: "WAKit",
-            dependencies: ["LEB"],
-            path: "./Sources/WAKit"
+            dependencies: [.product(name: "LEB", package: "SwiftLEB")]
         ),
         .testTarget(
             name: "WAKitTests",
-            dependencies: ["WAKit"],
-            path: "./Tests/WAKitTests"
+            dependencies: ["WAKit"]
         ),
         .target(
             name: "CLI",
-            dependencies: ["WAKit", "SwiftCLI", "Rainbow", "Logging"],
-            path: "./Sources/CLI"
+            dependencies: [
+                "WAKit",
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                "Rainbow",
+                .product(name: "Logging", package: "swift-log")
+            ]
         ),
     ],
     swiftLanguageVersions: [.v5]
--- Sources/CLI/SpecTest/Spectest.swift
+++ Sources/CLI/SpecTest/Spectest.swift
@@ -1,33 +1,37 @@
-import SwiftCLI
+import ArgumentParser
 
-final class SpecTestCommand: Command {
-    let name = "spectest"
+struct Spectest: ParsableCommand {
+    @Argument
+    var path: String
 
-    let path = Parameter()
-    let include = Key<String>("--include")
-    let exclude = Key<String>("--exclude")
-    let isVerbose = Flag("-v", "--verbose")
+    @Option
+    var include: String?
 
-    func execute() throws {
-        let isVerbose = self.isVerbose.value
-        let include = self.include.value.flatMap { $0.split(separator: ",").map(String.init) } ?? []
-        let exclude = self.exclude.value.flatMap { $0.split(separator: ",").map(String.init) } ?? []
+    @Option
+    var exclude: String?
+
+    @Flag
+    var verbose = false
+
+    func run() throws {
+        let include = self.include.flatMap { $0.split(separator: ",").map(String.init) } ?? []
+        let exclude = self.exclude.flatMap { $0.split(separator: ",").map(String.init) } ?? []
 
         let testCases: [TestCase]
         do {
-            testCases = try TestCase.load(include: include, exclude: exclude, in: path.value)
+            testCases = try TestCase.load(include: include, exclude: exclude, in: path)
         } catch {
             fatalError("failed to load test: \(error)")
         }
 
         var results = [Result]()
         for testCase in testCases {
-            testCase.run(rootPath: path.value) { testCase, command, result in
+            testCase.run(rootPath: path) { testCase, command, result in
                 switch result {
                 case let .failed(reason):
                     print("\(testCase.sourceFilename):\(command.line):", result.banner, reason)
                 case let .skipped(reason):
-                    if isVerbose { print("\(testCase.sourceFilename):\(command.line):", result.banner, reason) }
+                    if verbose { print("\(testCase.sourceFilename):\(command.line):", result.banner, reason) }
                 default:
                     print("\(testCase.sourceFilename):\(command.line):", result.banner)
                 }
