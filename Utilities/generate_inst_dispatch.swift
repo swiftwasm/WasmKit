2f39e21b3c4cc1582b608076a6b59c5d570eb009
Yuta Saito <kateinoigakukun@gmail.com>
2024-01-21 20:24:10 +0900

    doExecute is now well small to be inlined without @_transparent

--- Utilities/generate_inst_dispatch.swift
+++ Utilities/generate_inst_dispatch.swift
@@ -137,7 +137,6 @@ func camelCase(pascalCase: String) -> String {
 func generateDispatcher(instructions: [Instruction]) -> String {
     var output = """
     extension ExecutionState {
-        @_transparent
         mutating func doExecute(_ instruction: Instruction, runtime: Runtime) throws -> Bool {
             switch instruction {
     """
