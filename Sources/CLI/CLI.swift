842a3a4e161edc4f6fa6cf1c7131cf6a8e66aa8a
Yuta Saito <kateinoigakukun@gmail.com>
2024-07-28 17:10:46 +0900

    Bump version to 0.0.8

--- Sources/CLI/CLI.swift
+++ Sources/CLI/CLI.swift
@@ -5,7 +5,7 @@ struct CLI: AsyncParsableCommand {
     static let configuration = CommandConfiguration(
         commandName: "wasmkit",
         abstract: "WebAssembly Runtime written in Swift.",
-        version: "0.0.7",
+        version: "0.0.8",
         subcommands: [Run.self]
     )
 }
