8a53a805b0164b179db4a01be65eb1b58874fc84
Joe Heck <heckj@mac.com>
2024-08-07 10:09:53 -0700

    global variable initialization isn't allow with throw in swift6 compiler - adds boilerplate main() around the examples showing use of WasmKit

--- Examples/Sources/Factorial/Factorial.swift
+++ Examples/Sources/Factorial/Factorial.swift
@@ -4,15 +4,20 @@ import WasmKit
 import WAT
 import Foundation
 
-// Convert a WAT file to a Wasm binary, then parse it.
-let module = try parseWasm(
-    bytes: try wat2wasm(String(contentsOfFile: "wasm/factorial.wat"))
-)
+@main
+struct exampleExec {
+    static func main() throws {
+        // Convert a WAT file to a Wasm binary, then parse it.
+        let module = try parseWasm(
+            bytes: try wat2wasm(String(contentsOfFile: "wasm/factorial.wat"))
+        )
 
-// Create a module instance from the parsed module.
-let runtime = Runtime()
-let instance = try runtime.instantiate(module: module)
-let input: UInt64 = 5
-// Invoke the exported function "fac" with a single argument.
-let result = try runtime.invoke(instance, function: "fac", with: [.i64(input)])
-print("fac(\(input)) = \(result[0].i64)")
+        // Create a module instance from the parsed module.
+        let runtime = Runtime()
+        let instance = try runtime.instantiate(module: module)
+        let input: UInt64 = 5
+        // Invoke the exported function "fac" with a single argument.
+        let result = try runtime.invoke(instance, function: "fac", with: [.i64(input)])
+        print("fac(\(input)) = \(result[0].i64)")
+    }
+}
--- Examples/Sources/PrintAdd/PrintAdd.swift
+++ Examples/Sources/PrintAdd/PrintAdd.swift
@@ -3,30 +3,35 @@
 import WasmKit
 import WAT
 
-// Convert a WAT file to a Wasm binary, then parse it.
-let module = try parseWasm(
-    bytes: try wat2wasm(
-        """
-        (module
-          (import "printer" "print_i32" (func $print_i32 (param i32)))
-          (func (export "print_add") (param $x i32) (param $y i32)
-            (call $print_i32 (i32.add (local.get $x) (local.get $y)))
-          )
+@main
+struct exampleExec {
+    static func main() throws {
+        // Convert a WAT file to a Wasm binary, then parse it.
+        let module = try parseWasm(
+            bytes: try wat2wasm(
+                """
+                (module
+                  (import "printer" "print_i32" (func $print_i32 (param i32)))
+                  (func (export "print_add") (param $x i32) (param $y i32)
+                    (call $print_i32 (i32.add (local.get $x) (local.get $y)))
+                  )
+                )
+                """
+            )
         )
-        """
-    )
-)
 
-// Define a host function that prints an i32 value.
-let hostPrint = HostFunction(type: FunctionType(parameters: [.i32])) { _, args in
-    // This function is called from "print_add" in the WebAssembly module.
-    print(args[0])
-    return []
+        // Define a host function that prints an i32 value.
+        let hostPrint = HostFunction(type: FunctionType(parameters: [.i32])) { _, args in
+            // This function is called from "print_add" in the WebAssembly module.
+            print(args[0])
+            return []
+        }
+        // Create a runtime importing the host function.
+        let runtime = Runtime(hostModules: [
+            "printer": HostModule(functions: ["print_i32": hostPrint])
+        ])
+        let instance = try runtime.instantiate(module: module)
+        // Invoke the exported function "print_add"
+        _ = try runtime.invoke(instance, function: "print_add", with: [.i32(42), .i32(3)])
+    }
 }
-// Create a runtime importing the host function.
-let runtime = Runtime(hostModules: [
-    "printer": HostModule(functions: ["print_i32": hostPrint])
-])
-let instance = try runtime.instantiate(module: module)
-// Invoke the exported function "print_add"
-_ = try runtime.invoke(instance, function: "print_add", with: [.i32(42), .i32(3)])
