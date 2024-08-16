6e38141a2128304ef805dce203e16331c83bfc34
Yuta Saito <kateinoigakukun@gmail.com>
2024-08-16 06:18:56 +0000

    Setup execution fuzzing target

--- FuzzTesting/Package.swift
+++ FuzzTesting/Package.swift
@@ -6,12 +6,16 @@ let package = Package(
     name: "FuzzTesting",
     products: [
         .library(name: "FuzzTranslator", type: .static, targets: ["FuzzTranslator"]),
+        .library(name: "FuzzExecute", type: .static, targets: ["FuzzExecute"]),
     ],
     dependencies: [
         .package(path: "../"),
     ],
     targets: [
         .target(name: "FuzzTranslator", dependencies: [
+            .product(name: "WasmKit", package: "WasmKit")
+        ]),
+        .target(name: "FuzzExecute", dependencies: [
             .product(name: "WasmKit", package: "WasmKit")
         ]),
     ]
