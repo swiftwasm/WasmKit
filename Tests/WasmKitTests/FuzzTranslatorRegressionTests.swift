43375b79882424e2f45433fd6c40c65a5129f787
Yuta Saito <kateinoigakukun@gmail.com>
2024-08-24 23:15:25 +0900

    Improve test output for FuzzTranslatorRegressionTests

--- Tests/WasmKitTests/FuzzTranslatorRegressionTests.swift
+++ Tests/WasmKitTests/FuzzTranslatorRegressionTests.swift
@@ -3,13 +3,14 @@ import XCTest
 
 final class FuzzTranslatorRegressionTests: XCTestCase {
     func testRunAll() async throws {
-        let failCasesDir = URL(fileURLWithPath: #filePath)
+        let sourceRoot = URL(fileURLWithPath: #filePath)
             .deletingLastPathComponent().deletingLastPathComponent().deletingLastPathComponent()
+        let failCasesDir = sourceRoot
             .appendingPathComponent("FuzzTesting/FailCases/FuzzTranslator")
 
         for file in try FileManager.default.contentsOfDirectory(atPath: failCasesDir.path) {
             let path = failCasesDir.appendingPathComponent(file).path
-            print("Fuzz regression test: \(path)")
+            print("Fuzz regression test: \(path.dropFirst(sourceRoot.path.count + 1))")
 
             let data = try Data(contentsOf: URL(fileURLWithPath: path))
             do {
