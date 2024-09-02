3fa3a189ed957d1aba5c20e04f77b18e7ecd84d1
Yuta Saito <kateinoigakukun@gmail.com>
2024-09-02 22:42:01 +0900

    Check token threading model more

--- Tests/WasmKitTests/SpectestTests.swift
+++ Tests/WasmKitTests/SpectestTests.swift
@@ -7,34 +7,42 @@ final class SpectestTests: XCTestCase {
         .deletingLastPathComponent().deletingLastPathComponent().deletingLastPathComponent()
     static let testsuite = projectDir
         .appendingPathComponent("Vendor/testsuite")
+    static var testPaths: [String] {
+        [
+            Self.testsuite.path,
+            Self.testsuite.appendingPathComponent("proposals/memory64").path,
+            Self.projectDir.appendingPathComponent("Tests/WasmKitTests/ExtraSuite").path,
+        ]
+    }
 
     /// Run all the tests in the spectest suite.
     func testRunAll() async throws {
         let defaultConfig = RuntimeConfiguration()
         let environment = ProcessInfo.processInfo.environment
         let ok = try await spectest(
-            path: [
-                Self.testsuite.path,
-                Self.testsuite.appendingPathComponent("proposals/memory64").path,
-                Self.projectDir.appendingPathComponent("Tests/WasmKitTests/ExtraSuite").path,
-            ],
+            path: Self.testPaths,
             include: environment["WASMKIT_SPECTEST_INCLUDE"],
             exclude: environment["WASMKIT_SPECTEST_EXCLUDE"],
             parallel: true,
             configuration: defaultConfig
         )
         XCTAssertTrue(ok)
+    }
 
-        if defaultConfig.threadingModel != .token {
-            // Sanity check that non-default threading models work.
-            var config = defaultConfig
-            config.threadingModel = .token
-            let ok = try await spectest(
-                path: [Self.testsuite.appendingPathComponent("forward.wast").path,],
-                include: nil, exclude: nil,
-                configuration: config
-            )
-            XCTAssertTrue(ok)
-        }
+    func testRunAllWithTokenThreading() async throws {
+        let defaultConfig = RuntimeConfiguration()
+        guard defaultConfig.threadingModel != .token else { return }
+        // Sanity check that non-default threading models work.
+        var config = defaultConfig
+        let environment = ProcessInfo.processInfo.environment
+        config.threadingModel = .token
+        let ok = try await spectest(
+            path: Self.testPaths,
+            include: environment["WASMKIT_SPECTEST_INCLUDE"],
+            exclude: environment["WASMKIT_SPECTEST_EXCLUDE"],
+            parallel: true,
+            configuration: config
+        )
+        XCTAssertTrue(ok)
     }
 }
