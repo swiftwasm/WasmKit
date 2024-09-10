130fb439e1ea36d3ad37e1422b0102c6d8e8a0be
Yuta Saito <kateinoigakukun@gmail.com>
2024-09-10 09:44:11 +0300

    Reduce integer casting

--- Sources/WasmKit/Execution/Instructions/Parametric.swift
+++ Sources/WasmKit/Execution/Instructions/Parametric.swift
@@ -2,7 +2,7 @@
 /// <https://webassembly.github.io/spec/core/exec/instructions.html#parametric-instructions>
 extension ExecutionState {
     mutating func select(sp: Sp, selectOperand: Instruction.SelectOperand) {
-        let flag = sp[selectOperand.condition].i32
+        let flag = sp[i32: selectOperand.condition]
         let selected = flag != 0 ? selectOperand.onTrue : selectOperand.onFalse
         let value = sp[selected]
         sp[selectOperand.result] = value
