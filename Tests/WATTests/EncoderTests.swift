a8e5ddd65d1f4c4ccb47c27f112578b5c7701f93
Yuta Saito <kateinoigakukun@gmail.com>
2024-10-03 09:38:05 +0900

    WAT: Ban multiple start sections

--- Tests/WATTests/EncoderTests.swift
+++ Tests/WATTests/EncoderTests.swift
@@ -107,7 +107,6 @@ class EncoderTests: XCTestCase {
 
             var stats = CompatibilityTestStats()
             let excluded: [String] = [
-                "start.wast",
                 "token.wast",
                 "utf8-invalid-encoding.wast"
             ]
