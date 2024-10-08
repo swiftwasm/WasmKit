813e425988f12b5d9801b22210b2bc85f4117e9e
Yuta Saito <kateinoigakukun@gmail.com>
2024-10-08 15:31:11 +0900

    Move `EngineInterceptor` to a separate file
    The file was created but the code was not moved to it

--- Sources/WasmKit/Engine.swift
+++ Sources/WasmKit/Engine.swift
@@ -96,36 +96,6 @@ public struct EngineConfiguration {
     }
 }
 
-@_documentation(visibility: internal)
-public protocol EngineInterceptor {
-    func onEnterFunction(_ function: Function)
-    func onExitFunction(_ function: Function)
-}
-
-/// An interceptor that multiplexes multiple interceptors
-@_documentation(visibility: internal)
-public class MultiplexingInterceptor: EngineInterceptor {
-    private let interceptors: [EngineInterceptor]
-
-    /// Creates a new multiplexing interceptor
-    /// - Parameter interceptors: The interceptors to multiplex
-    public init(_ interceptors: [EngineInterceptor]) {
-        self.interceptors = interceptors
-    }
-
-    public func onEnterFunction(_ function: Function) {
-        for interceptor in interceptors {
-            interceptor.onEnterFunction(function)
-        }
-    }
-
-    public func onExitFunction(_ function: Function) {
-        for interceptor in interceptors {
-            interceptor.onExitFunction(function)
-        }
-    }
-}
-
 extension Engine {
     func resolveType(_ type: InternedFuncType) -> FunctionType {
         return funcTypeInterner.resolve(type)
