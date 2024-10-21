33c6999eb0cdce9780d6ae65337db2fda1d5b8dc
Yuta Saito <kateinoigakukun@gmail.com>
2024-10-21 21:41:40 +0900

    Test backtrace attached to Trap

--- Tests/WasmKitTests/ExecutionTests.swift
+++ Tests/WasmKitTests/ExecutionTests.swift
@@ -51,4 +51,49 @@ final class ExecutionTests: XCTestCase {
         let results = try _start()
         XCTAssertEqual(results, [.i32(42)])
     }
+
+    func testBacktrace() throws {
+        let module = try parseWasm(
+            bytes: wat2wasm(
+                """
+                (module
+                  (memory (export "memory") 1)
+                  (func $foo (result i32)
+                    unreachable
+                  )
+                  (func $bar (result i32)
+                    (call $foo)
+                  )
+                  (func (export "_start")
+                    (call $bar)
+                    (drop)
+                  )
+                )
+                """,
+                options: EncodeOptions(nameSection: true)
+            )
+        )
+
+        let engine = Engine()
+        let store = Store(engine: engine)
+        let instance = try module.instantiate(store: store)
+        let _start = try XCTUnwrap(instance.exports[function: "_start"])
+
+        let trap: Trap
+        do {
+            try _start()
+            XCTFail("expect unreachable trap")
+            return
+        } catch let error {
+            trap = try XCTUnwrap(error as? Trap)
+        }
+
+        XCTAssertEqual(
+            trap.backtrace?.symbols.compactMap(\.?.name),
+            [
+                "foo",
+                "bar",
+                "_start",
+            ])
+    }
 }
