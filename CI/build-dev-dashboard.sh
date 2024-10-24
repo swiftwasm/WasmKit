559ec832a46e5ea6cf315568d72b22f95dfa639c
Yuta Saito <kateinoigakukun@gmail.com>
2024-10-24 15:13:24 +0900

    CI: Fix build-dev-dashboard.sh script for Linux toolchain

--- CI/build-dev-dashboard.sh
+++ CI/build-dev-dashboard.sh
@@ -1,8 +1,21 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
+show_test_executable_path() {
+  local base_path=".build/debug/WasmKitPackageTests.xctest"
+  if [ -f "$base_path" ]; then
+    echo "$base_path"
+  else
+    echo "$base_path/Contents/MacOS/WasmKitPackageTests"
+  fi
+}
+
 build_coverage_html() {
-  llvm-cov show --format=html .build/debug/WasmKitPackageTests.xctest/Contents/MacOS/WasmKitPackageTests --instr-profile .build/debug/codecov/default.profdata -o "$1" --sources $(find Sources/ -type f)
+  llvm-cov show --format=html "$(show_test_executable_path)" --instr-profile .build/debug/codecov/default.profdata -o "$1" --sources $(find Sources -type f)
 }
 
 mkdir -p ./.build/html
 build_coverage_html ./.build/html/coverage
+
+echo "Coverage report has been generated in ./.build/html/coverage"
