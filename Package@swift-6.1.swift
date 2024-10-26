ee4a7177797c14e99e529ef499060eca1a880711
Yuta Saito <kateinoigakukun@gmail.com>
2024-10-26 19:37:22 +0900

    Sync recent Package.swift changes with the 6.1 manifest (#159)

--- Package@swift-6.1.swift
+++ Package@swift-6.1.swift
@@ -51,9 +51,14 @@ let package = Package(
             exclude: ["CMakeLists.txt"]
         ),
         .target(name: "_CWasmKit"),
+        .target(
+            name: "WasmKitFuzzing",
+            dependencies: ["WasmKit"],
+            path: "FuzzTesting/Sources/WasmKitFuzzing"
+        ),
         .testTarget(
             name: "WasmKitTests",
-            dependencies: ["WasmKit", "WAT"],
+            dependencies: ["WasmKit", "WAT", "WasmKitFuzzing"],
             exclude: ["ExtraSuite"]
         ),
 
