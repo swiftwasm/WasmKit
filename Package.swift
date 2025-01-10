ad80253f8d67644e9cb71f9870bc0c4792b3b546
Yuta Saito <kateinoigakukun@gmail.com>
2025-01-10 22:48:40 +0900

    Relax swift-system version requirement

--- Package.swift
+++ Package.swift
@@ -122,7 +122,7 @@ let package = Package(
 if ProcessInfo.processInfo.environment["SWIFTCI_USE_LOCAL_DEPS"] == nil {
     package.dependencies += [
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.2.2"),
-        .package(url: "https://github.com/apple/swift-system", .upToNextMinor(from: "1.3.0")),
+        .package(url: "https://github.com/apple/swift-system", from: "1.4.0"),
     ]
 } else {
     package.dependencies += [
