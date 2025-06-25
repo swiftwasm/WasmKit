63c75bb4e6c76c3ff72aa1921b99de457ead70b9
Max Desiatov <m_desiatov@apple.com>
2025-06-25 16:11:42 +0100

    Bump macOS version requirement, mark Windows unsupported

--- Sources/WASI/Poll.swift
+++ Sources/WASI/Poll.swift
@@ -26,21 +26,25 @@ func poll(
     subscriptions: some Sequence<WASIAbi.Subscription>,
     _ fdTable: FdTable
 ) throws {
-    var pollfds = [pollfd]()
-    var timeoutMilliseconds = UInt.max
+    #if os(Windows)
+        throw WASIAbi.Errno.ENOTSUP
+    #else
+        var pollfds = [pollfd]()
+        var timeoutMilliseconds = UInt.max
 
-    for subscription in subscriptions {
-        let union = subscription.union
-        switch union {
-        case .clock(let clock):
-            timeoutMilliseconds = min(timeoutMilliseconds, .init(clock.timeout / 1_000_000))
-        case .fdRead(let fd):
-            pollfds.append(.init(fd: try fdTable.fileDescriptor(fd: fd).rawValue, events: .init(POLLIN), revents: 0))
-        case .fdWrite(let fd):
-            pollfds.append(.init(fd: try fdTable.fileDescriptor(fd: fd).rawValue, events: .init(POLLOUT), revents: 0))
+        for subscription in subscriptions {
+            let union = subscription.union
+            switch union {
+            case .clock(let clock):
+                timeoutMilliseconds = min(timeoutMilliseconds, .init(clock.timeout / 1_000_000))
+            case .fdRead(let fd):
+                pollfds.append(.init(fd: try fdTable.fileDescriptor(fd: fd).rawValue, events: .init(POLLIN), revents: 0))
+            case .fdWrite(let fd):
+                pollfds.append(.init(fd: try fdTable.fileDescriptor(fd: fd).rawValue, events: .init(POLLOUT), revents: 0))
 
+            }
         }
-    }
 
-    poll(&pollfds, .init(pollfds.count), .init(timeoutMilliseconds))
+        poll(&pollfds, .init(pollfds.count), .init(timeoutMilliseconds))
+    #endif
 }
