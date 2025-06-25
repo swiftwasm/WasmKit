df29b59e4703a9596723b61b3810be93c33a1c6c
Max Desiatov <m_desiatov@apple.com>
2025-06-25 15:50:52 +0100

    Add `Poll.swift`
    Implementation of `poll_oneoff` in WebAssembly System Interface standard (https://github.com/WebAssembly/WASI/blob/main/legacy/preview1/docs.md#poll_oneoff) can be approximated in operating systems implementing POSIX standard with `poll` (https://www.unix.com/man_page/posix/2/ppoll).
    
    This change adds such approximation, converting clock subscriptions using nanoseconds to milliseconds, while mapping file descriptor read and write subscriptions to `POLLIN` and `POLLOUT` events in POSIX.

---
+++ Sources/WASI/Poll.swift
@@ -0,0 +1,46 @@
+#if canImport(Darwin)
+import Darwin
+#elseif canImport(Glibc)
+import Glibc
+#elseif canImport(Musl)
+import Musl
+#elseif canImport(Android)
+import Android
+#else
+#error("Unsupported Platform")
+#endif
+
+import SystemPackage
+
+extension FdTable {
+    func fileDescriptor(fd: WASIAbi.Fd) throws -> FileDescriptor {
+        guard case let .file(entry) = self[fd], let fd = (entry as? FdWASIEntry)?.fd  else {
+            throw WASIAbi.Errno.EBADF
+        }
+
+        return fd
+    }
+}
+
+func poll(
+    subscriptions: some Sequence<WASIAbi.Subscription>,
+    _ fdTable: FdTable
+) throws {
+    var pollfds = [pollfd]()
+    var timeoutMilliseconds = UInt.max
+
+    for subscription in subscriptions {
+        let union = subscription.union
+        switch union {
+        case .clock(let clock):
+            timeoutMilliseconds = min(timeoutMilliseconds, .init(clock.timeout / 1_000_000))
+        case .fdRead(let fd):
+            pollfds.append(.init(fd: try fdTable.fileDescriptor(fd: fd).rawValue, events: .init(POLLIN), revents: 0))
+        case .fdWrite(let fd):
+            pollfds.append(.init(fd: try fdTable.fileDescriptor(fd: fd).rawValue, events: .init(POLLOUT), revents: 0))
+
+        }
+    }
+
+    poll(&pollfds, .init(pollfds.count), .init(timeoutMilliseconds))
+}
