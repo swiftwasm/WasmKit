9e8306e9d4dee48c3c4cce62142046dd7428d178
Max Desiatov <m_desiatov@apple.com>
2025-06-25 16:24:41 +0100

    Exclude unsupported test on Windows

--- Tests/WASITests/WASITests.swift
+++ Tests/WASITests/WASITests.swift
@@ -173,10 +173,13 @@ final class WASITests: XCTestCase {
         XCTAssertEqual(pointer.offset, eventOffset)
         XCTAssertEqual(WASIAbi.Event.readFromGuest(&pointer), event)
         XCTAssertEqual(pointer.offset, finalOffset)
-        XCTAssertTrue(
-            try ContinuousClock().measure {
-                let clockPointer = UnsafeGuestBufferPointer<WASIAbi.Subscription>(baseAddress: .init(memorySpace: memory, offset: clockOffset), count: 1)
-                XCTAssertEqual(try WASIBridgeToHost().poll_oneoff(subscriptions: clockPointer, events: .init(baseAddress: .init(memorySpace: memory, offset: finalOffset), count: 1)), 1)
-            } > .nanoseconds(timeout))
+
+        #if !os(Windows)
+            XCTAssertTrue(
+                try ContinuousClock().measure {
+                    let clockPointer = UnsafeGuestBufferPointer<WASIAbi.Subscription>(baseAddress: .init(memorySpace: memory, offset: clockOffset), count: 1)
+                    XCTAssertEqual(try WASIBridgeToHost().poll_oneoff(subscriptions: clockPointer, events: .init(baseAddress: .init(memorySpace: memory, offset: finalOffset), count: 1)), 1)
+                } > .nanoseconds(timeout))
+        #endif
     }
 }
