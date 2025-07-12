448d1f82031ee59ceedecf49393010f2cdc72454
Yuta Saito <kateinoigakukun@gmail.com>
2025-07-12 18:20:22 +0000

    Strip debug symbols from the release ELF binary

--- Utilities/build-release.py
+++ Utilities/build-release.py
@@ -14,12 +14,20 @@ def run(arguments):
     print("Running: " + " ".join(arguments))
     subprocess.run(arguments, check=True)
 
+
+def is_elf(file_path):
+    """Check if the file is an ELF binary."""
+    with open(file_path, 'rb') as f:
+        header = f.read(4)
+    return header == b'\x7fELF'
+
+
 def main():
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument("-o", "--output", required=True)
     parser.add_argument("extra_build_args", nargs="*")
-    
+
     args = parser.parse_args()
 
     build_args = ["swift", "build", "-c", "release", "--product", "wasmkit-cli", "--package-path", SOURCE_ROOT] + args.extra_build_args
@@ -33,8 +41,15 @@ def main():
     shutil.rmtree(archive_path, ignore_errors=True)
     os.makedirs(archive_path)
 
+    src_exe_path = os.path.join(bin_path, "wasmkit-cli")
     dest_exe_path = os.path.join(archive_path, "wasmkit")
-    shutil.copy(os.path.join(bin_path, "wasmkit-cli"), dest_exe_path)
+    if is_elf(src_exe_path):
+        # For ELF binaries, use strip to remove debug symbols because
+        # most of static archives in static linux Swift SDK contains
+        # debug sections.
+        run(["strip", src_exe_path, "--strip-debug", "-o", dest_exe_path])
+    else:
+        shutil.copy(src_exe_path, dest_exe_path)
 
     with tarfile.open(args.output, "w:gz") as tar:
         tar.add(archive_path, arcname=os.path.basename(archive_path))
