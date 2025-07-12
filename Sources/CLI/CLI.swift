997c558f34fca52e3ab0a96cfe8c52a724c19bd1
Yuta Saito <kateinoigakukun@gmail.com>
2025-07-12 18:21:27 +0000

    Bump version to 0.1.6

--- Sources/CLI/CLI.swift
+++ Sources/CLI/CLI.swift
@@ -5,7 +5,7 @@ struct CLI: ParsableCommand {
     static let configuration = CommandConfiguration(
         commandName: "wasmkit",
         abstract: "WasmKit WebAssembly Runtime",
-        version: "0.1.5",
+        version: "0.1.6",
         subcommands: [Run.self, Explore.self]
     )
 }
