cf1359b725197d0dccc98e74b0b6b15f7cc8ff28
Max Desiatov <m_desiatov@apple.com>
2025-10-09 20:42:55 +0100

    Update doc comment of `func instantiate` in `Module.swift`

--- Sources/WasmKit/Module.swift
+++ Sources/WasmKit/Module.swift
@@ -145,6 +145,8 @@ public struct Module {
         ///   - store: The ``Store`` to allocate the instance in.
         ///   - imports: The imports to use for instantiation. All imported entities
         ///     must be allocated in the given store.
+        ///   - isDebuggable: Whether the module should support debugging actions
+        ///     (breakpoints etc) after instantiation.
         public func instantiate(store: Store, imports: Imports = [:], isDebuggable: Bool) throws -> Instance {
             Instance(handle: try self.instantiateHandle(store: store, imports: imports, isDebuggable: isDebuggable), store: store)
         }
