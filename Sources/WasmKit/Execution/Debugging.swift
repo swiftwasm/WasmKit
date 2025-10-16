806e7eccdff8cc4e1acf5f6f993ed282f765d3c7
Max Desiatov <m_desiatov@apple.com>
2025-10-16 13:08:54 +0100

    Add `DebuggerExecution` wrapper type

---
+++ Sources/WasmKit/Execution/Debugging.swift
@@ -0,0 +1,21 @@
+package struct DebuggerExecution: ~Copyable {
+    let valueStack: Sp
+    let execution: Execution
+    let store: Store
+
+    package init(store: Store) {
+        let limit = store.engine.configuration.stackSize / MemoryLayout<StackSlot>.stride
+        self.valueStack = UnsafeMutablePointer<StackSlot>.allocate(capacity: limit)
+        self.store = store
+        self.execution = Execution(store: StoreRef(store), stackEnd: valueStack.advanced(by: limit))
+    }
+
+    package func captureBacktrace() -> Backtrace {
+            return Execution.captureBacktrace(sp: self.valueStack, store: self.store)
+    }
+
+    deinit {
+        valueStack.deallocate()
+    }
+}
+
