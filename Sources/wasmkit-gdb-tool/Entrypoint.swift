4ea0fb0fb55fd6199ce63381083e1a6bc8496a81
Max Desiatov <m_desiatov@apple.com>
2025-10-17 16:33:34 +0100

    Fix formatting

--- Sources/wasmkit-gdb-tool/Entrypoint.swift
+++ Sources/wasmkit-gdb-tool/Entrypoint.swift
@@ -40,71 +40,71 @@ struct Entrypoint: AsyncParsableCommand {
     var wasmModulePath: FilePath
 
     func run() async throws {
-#if WasmDebuggingSupport
-        let logger = {
-            var result = Logger(label: "org.swiftwasm.WasmKit")
-            result.logLevel = self.logLevel
-            return result
-        }()
+        #if WasmDebuggingSupport
+            let logger = {
+                var result = Logger(label: "org.swiftwasm.WasmKit")
+                result.logLevel = self.logLevel
+                return result
+            }()
 
-        try await MultiThreadedEventLoopGroup.withEventLoopGroup(numberOfThreads: System.coreCount) { group in
-            let bootstrap = ServerBootstrap(group: group)
-                // Specify backlog and enable SO_REUSEADDR for the server itself
-                .serverChannelOption(.backlog, value: 256)
-                .serverChannelOption(.socketOption(.so_reuseaddr), value: 1)
+            try await MultiThreadedEventLoopGroup.withEventLoopGroup(numberOfThreads: System.coreCount) { group in
+                let bootstrap = ServerBootstrap(group: group)
+                    // Specify backlog and enable SO_REUSEADDR for the server itself
+                    .serverChannelOption(.backlog, value: 256)
+                    .serverChannelOption(.socketOption(.so_reuseaddr), value: 1)
 
-                // Set the handlers that are applied to the accepted child `Channel`s.
-                .childChannelInitializer { channel in
-                    // Ensure we don't read faster then we can write by adding the BackPressureHandler into the pipeline.
-                    channel.eventLoop.makeCompletedFuture {
-                        try channel.pipeline.syncOperations.addHandler(BackPressureHandler())
-                        // make sure to instantiate your `ChannelHandlers` inside of
-                        // the closure as it will be invoked once per connection.
-                        try channel.pipeline.syncOperations.addHandlers([
-                            ByteToMessageHandler(GDBHostCommandDecoder(logger: logger)),
-                            MessageToByteHandler(GDBTargetResponseEncoder()),
-                        ])
+                    // Set the handlers that are applied to the accepted child `Channel`s.
+                    .childChannelInitializer { channel in
+                        // Ensure we don't read faster then we can write by adding the BackPressureHandler into the pipeline.
+                        channel.eventLoop.makeCompletedFuture {
+                            try channel.pipeline.syncOperations.addHandler(BackPressureHandler())
+                            // make sure to instantiate your `ChannelHandlers` inside of
+                            // the closure as it will be invoked once per connection.
+                            try channel.pipeline.syncOperations.addHandlers([
+                                ByteToMessageHandler(GDBHostCommandDecoder(logger: logger)),
+                                MessageToByteHandler(GDBTargetResponseEncoder()),
+                            ])
+                        }
+                    }
+
+                    // Enable SO_REUSEADDR for the accepted Channels
+                    .childChannelOption(.socketOption(.so_reuseaddr), value: 1)
+                    .childChannelOption(.maxMessagesPerRead, value: 16)
+                    .childChannelOption(.recvAllocator, value: AdaptiveRecvByteBufferAllocator())
+
+                let serverChannel = try await bootstrap.bind(host: "127.0.0.1", port: port) { childChannel in
+                    childChannel.eventLoop.makeCompletedFuture {
+                        try NIOAsyncChannel<GDBPacket<GDBHostCommand>, GDBTargetResponse>(
+                            wrappingChannelSynchronously: childChannel
+                        )
                     }
                 }
+                /* the server will now be accepting connections */
+                logger.info("listening on port \(port)")
 
-                // Enable SO_REUSEADDR for the accepted Channels
-                .childChannelOption(.socketOption(.so_reuseaddr), value: 1)
-                .childChannelOption(.maxMessagesPerRead, value: 16)
-                .childChannelOption(.recvAllocator, value: AdaptiveRecvByteBufferAllocator())
+                let debugger = try await WasmKitGDBHandler(logger: logger, moduleFilePath: self.wasmModulePath)
 
-            let serverChannel = try await bootstrap.bind(host: "127.0.0.1", port: port) { childChannel in
-                childChannel.eventLoop.makeCompletedFuture {
-                    try NIOAsyncChannel<GDBPacket<GDBHostCommand>, GDBTargetResponse>(
-                        wrappingChannelSynchronously: childChannel
-                    )
-                }
-            }
-            /* the server will now be accepting connections */
-            logger.info("listening on port \(port)")
-
-            let debugger = try await WasmKitGDBHandler(logger: logger, moduleFilePath: self.wasmModulePath)
-
-            try await withThrowingDiscardingTaskGroup { group in
-                try await serverChannel.executeThenClose { serverChannelInbound in
-                    for try await connectionChannel in serverChannelInbound {
-                        group.addTask {
-                            do {
-                                try await connectionChannel.executeThenClose { connectionChannelInbound, connectionChannelOutbound in
-                                    for try await inboundData in connectionChannelInbound {
-                                        // Let's echo back all inbound data
-                                        try await connectionChannelOutbound.write(debugger.handle(command: inboundData.payload))
+                try await withThrowingDiscardingTaskGroup { group in
+                    try await serverChannel.executeThenClose { serverChannelInbound in
+                        for try await connectionChannel in serverChannelInbound {
+                            group.addTask {
+                                do {
+                                    try await connectionChannel.executeThenClose { connectionChannelInbound, connectionChannelOutbound in
+                                        for try await inboundData in connectionChannelInbound {
+                                            // Let's echo back all inbound data
+                                            try await connectionChannelOutbound.write(debugger.handle(command: inboundData.payload))
+                                        }
                                     }
+                                } catch {
+                                    logger.error("Error in GDB remote protocol connection channel", metadata: ["error": "\(error)"])
                                 }
-                            } catch {
-                                logger.error("Error in GDB remote protocol connection channel", metadata: ["error": "\(error)"])
                             }
                         }
                     }
                 }
             }
-        }
-#else
-        fatalError("Build WasmKit with `WasmDebuggingSupport` trait")
-#endif
+        #else
+            fatalError("Build WasmKit with `WasmDebuggingSupport` trait")
+        #endif
     }
 }
