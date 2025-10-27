ad3c20c3767847d5d1ee9764b23b74a271baf7cf
Max Desiatov <m_desiatov@apple.com>
2025-10-27 16:50:42 +0000

    Implement Android trait condition in `WASITests`

--- Tests/WASITests/WASITests.swift
+++ Tests/WASITests/WASITests.swift
@@ -2,6 +2,33 @@ import Testing
 
 @testable import WASI
 
+enum TargetPlatform {
+    case android
+
+    func isCurrentPlatform() -> Bool {
+        switch self {
+        case .android:
+            #if os(Android)
+                return true
+            #else
+                return false
+            #endif
+        }
+    }
+}
+
+extension Trait where Self == ConditionTrait {
+    static func disabled(
+        _ comment: Comment? = nil,
+        sourceLocation: SourceLocation = #_sourceLocation,
+        platforms: [TargetPlatform]
+    ) -> Self {
+        return .disabled(comment, sourceLocation: sourceLocation) {
+            platforms.contains { $0.isCurrentPlatform() }
+        }
+    }
+}
+
 @Suite
 struct WASITests {
     #if !os(Windows)
