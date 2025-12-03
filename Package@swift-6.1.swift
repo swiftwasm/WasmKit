4b6dfc0f377efa81e2afdd29bca26351095e9e87
Max Desiatov <m_desiatov@apple.com>
2025-12-03 14:40:11 +0000

    Use lower Darwin platform versions in `Package@swift-6.1.swift`
    The current platform versions are set to values higher than necessary, and we should support older platforms for as long as possible with our current implementation.

--- Package@swift-6.1.swift
+++ Package@swift-6.1.swift
@@ -20,7 +20,7 @@ let cliTarget = Target.executableTarget(
 
 let package = Package(
     name: "WasmKit",
-    platforms: [.macOS(.v15), .iOS(.v17)],
+    platforms: [.macOS(.v14), .iOS(.v12)],
     products: [
         .executable(name: "wasmkit-cli", targets: ["CLI"]),
         .library(name: "WasmKit", targets: ["WasmKit"]),
