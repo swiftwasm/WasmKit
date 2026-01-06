99dd706666b183047ebfdb16e676429c6e48b78a
Max Desiatov <m_desiatov@apple.com>
2026-01-06 16:54:32 +0000

    Add support for Linux in `LLVMBackend/Linker.swift`

--- Sources/LLVMBackend/Linker.swift
+++ Sources/LLVMBackend/Linker.swift
@@ -7,26 +7,35 @@ package enum Linker {
     }
 
     package static func link(objectFilePath: FilePath) async throws -> FilePath {
+        var dylibPath = objectFilePath
+        let arguments: Arguments
         #if os(macOS)
-            var dylibPath = objectFilePath
             dylibPath.extension = "dylib"
-            let result = try await run(
-                .name("ld"),
-                arguments: [
-                    "-dylib", objectFilePath.string,
-                    "-lSystem", "-L", "/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib",
-                    "-o", dylibPath.string,
-                ],
-                output: .standardError
-            )
-
-            guard result.terminationStatus.isSuccess else {
-                throw Error.unexpectedTerminationStatus(result.terminationStatus)
-            }
-
-            return dylibPath
+            arguments = [
+                "-dylib", objectFilePath.string,
+                "-lSystem", "-L", "/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib",
+                "-o", dylibPath.string,
+            ]
+        #elseif os(Linux)
+            dylibPath.extension = "so"
+            arguments = [
+                "-shared", objectFilePath.string,
+                "-o", dylibPath.string,
+            ]
         #else
-            fatalError("Linking native binaries is currently only supported on macOS")
+            #error("Linking native binaries is currently only supported on macOS and Linux")
         #endif
+
+        let result = try await run(
+            .name("ld"),
+            arguments: arguments,
+            output: .standardError
+        )
+
+        guard result.terminationStatus.isSuccess else {
+            throw Error.unexpectedTerminationStatus(result.terminationStatus)
+        }
+
+        return dylibPath
     }
 }
