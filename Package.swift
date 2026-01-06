61454ea706e925b6b1705b8cb26c0e18d5a4fe91
Max Desiatov <m_desiatov@apple.com>
2026-01-06 15:52:33 +0000

    Fix missing comma in `Package.swift`

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
             name: "CLI",
             dependencies: [
                 "CLICommands",
-            ]
+            ],
             exclude: ["CMakeLists.txt"]
         ),
 
