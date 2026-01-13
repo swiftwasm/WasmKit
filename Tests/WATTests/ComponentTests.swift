1bfb918a47f659bbb1b3dc311b54df296edaae37
Max Desiatov <m_desiatov@apple.com>
2026-01-13 09:30:24 +0000

    Attempt parsing core module instances

--- Tests/WATTests/ComponentTests.swift
+++ Tests/WATTests/ComponentTests.swift
@@ -16,11 +16,11 @@ struct ComponentTests {
           (core module)
         )
         (component $bar
-          (core module
+          (core module $m1
             (func (export "a") (result i32) i32.const 0)
             (func (export "b") (result i64) i64.const 0)
           )
-          (core module
+          (core module $m2
             (func (export "c") (result f32) f32.const 0)
             (func (export "d") (result f64) f64.const 0)
           )
@@ -30,7 +30,7 @@ struct ComponentTests {
         var accummulated = [String?]()
         while let field = try parser.next() {
             switch field.kind {
-            case .component(let name, let decls):
+            case .component(let name, _):
                 accummulated.append(name?.value)
             case _:
                 #expect((false), "Expected only component directive")
