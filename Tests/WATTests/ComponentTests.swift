88ed2ba28ef25aa3f0fdee9e8a552f694a8f910e
Max Desiatov <m_desiatov@apple.com>
2026-01-13 09:18:06 +0000

    Implement parsing core modules

--- Tests/WATTests/ComponentTests.swift
+++ Tests/WATTests/ComponentTests.swift
@@ -10,18 +10,33 @@ struct ComponentTests {
         #"""
         (component)
         (component $foo)
+        (component
+          (core module)
+          (core module)
+          (core module)
+        )
+        (component $bar
+          (core module
+            (func (export "a") (result i32) i32.const 0)
+            (func (export "b") (result i64) i64.const 0)
+          )
+          (core module
+            (func (export "c") (result f32) f32.const 0)
+            (func (export "d") (result f64) f64.const 0)
+          )
+        )
         """#, features: .default)
 
         var accummulated = [String?]()
         while let field = try parser.next() {
             switch field.kind {
-            case .component(let name):
+            case .component(let name, let decls):
                 accummulated.append(name?.value)
             case _:
                 #expect((false), "Expected only component directive")
             }
         }
 
-        #expect(accummulated == [nil, "$foo"])
+        #expect(accummulated == [nil, "$foo", nil, "$bar"])
     }
 }
