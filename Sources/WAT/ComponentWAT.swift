e9c1ff2e14e6ded2ac063bf98bfebfa32408fb1a
Max Desiatov <m_desiatov@apple.com>
2026-01-16 11:06:25 +0000

    Add basic support for `canon` keyword parsing

--- Sources/WAT/ComponentWAT.swift
+++ Sources/WAT/ComponentWAT.swift
@@ -4,13 +4,14 @@ import WasmParser
 
 /// https://github.com/WebAssembly/component-model/blob/main/design/mvp/Explainer.md#index-spaces
 package struct ComponentWat {
-    let functionsMap: NameMapping<ComponentWatParser.FunctionDef>
     let valuesMap: NameMapping<ComponentWatParser.ValueDef>
     let typesMap: NameMapping<ComponentWatParser.TypeDef>
     let componentInstancesMap: NameMapping<ComponentWatParser.ComponentInstanceDef>
     let componentsMap: NameMapping<ComponentWatParser.ComponentDef>
     let modulesMap: NameMapping<ComponentWatParser.ModuleDef>
     let coreInstancesMap: NameMapping<ComponentWatParser.CoreInstanceDef>
+    let canonLiftsMap: NameMapping<ComponentWatParser.CanonDef>
+    let canonLowersMap: NameMapping<ComponentWatParser.CanonDef>
 }
 
 #endif
