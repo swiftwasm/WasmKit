d968b20083277fd71d091debf98f0ceedff46e8d
Max Desiatov <m_desiatov@apple.com>
2026-02-06 16:55:55 +0000

    Disable test failing with the latest spec for now

--- Tests/WATTests/ComponentTests.swift
+++ Tests/WATTests/ComponentTests.swift
@@ -69,7 +69,6 @@
                     "link.wast",
                     "inline-exports.wast",
                     "nested-modules.wast",
-                    "definedtypes.wast",
                     "fused.wast",
                 ],
                 excludePaths: []
