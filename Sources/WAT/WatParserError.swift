2e38f005c28f364c4a5a7b385df5cf87e0d2292e
Max Desiatov <m_desiatov@apple.com>
2026-07-10 14:31:35 +0100

    Apply suggestions from code review
    Co-authored-by: Max Desiatov <m_desiatov@apple.com>

--- Sources/WAT/WatParserError.swift
+++ Sources/WAT/WatParserError.swift
@@ -1,6 +1,6 @@
 /// An error type thrown during WAT (WebAssembly Text Format) parsing.
 public struct WatParserError: Swift.Error {
-    public let message: String
+    package let message: String
     public let location: Location?
 
     package init(_ message: String, location: Location?) {
